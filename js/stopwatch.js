b90084324d82eefb0e1c1bb6954b090b69d806b9
Brian Hsu <brianhsu@berkeley.edu>
2018-09-26 00:54:07 -0700

    Added stopwatch.js with basic implementation

---
+++ js/stopwatch.js
@@ -0,0 +1,55 @@
+/**
+ * Stopwatch object, supporting starting, stopping and updating.
+ */
+var Stopwatch = function () {
+
+    var startTime;
+    var endTime;
+    var running = false;
+
+    this.start = function() {
+        startTime = new Date().getTime();
+    };
+
+    this.stop = function() {
+        endTime = new Date().getTime();
+        running = false;
+    };
+
+    /**
+     * Returns the time elapsed since the start button was clicked, formatted in HH:MM:SS form.
+     */
+    this.update = function() {
+        if (running) {
+            var currentTime = new Date().getTime();
+            var elapsed = Math.floor((currentTimeTime - startTime) / 1000);
+            return this.formatTime(elapsed);
+        }
+    };
+
+    /**
+     * Returns the elapsed time formatted in HH:MM:SS
+     * @param elapsed: The time elapsed in seconds.
+     */
+    this.formatTime = function(elapsed) {
+        var hours = Math.floor(elapsed / (60 * 60));
+        var minutes = Math.floor(elapsed % (60 * 60) / (60 * 60));
+        var seconds = Math.floor(elapsed % (60 * 60 * 60));
+        if (hours === 0) {
+            hours = "00"
+        } else if (hours < 10) {
+            hours = "0" + hours.toString();
+        }
+        if (minutes === 0) {
+            minutes = "00";
+        } else if (minutes < 10) {
+            minutes = "0" + minutes.toString();
+        }
+        if (seconds === 0) {
+            seconds = "00";
+        } else if (seconds < 10) {
+            seconds = "0" + seconds.toString();
+        }
+        return hours + ":" + minutes + ":" + seconds
+    }
+};
